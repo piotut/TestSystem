0b6feca1f480da846fa0197569a3155492d2fb97
szwagier90 <szwagier90@gmail.com>
2015-04-21 01:21:19 +0200

    Login required to upload

--- testownik/urls.py
+++ testownik/urls.py
@@ -3,15 +3,16 @@ from django.conf.urls import url
 from .views import IndexView, ChooseTestView, LoginView, SheetView, UploadFileView, UserCreationView, PdfGeneratorView
 from django.views.generic import TemplateView
 
+from django.contrib.auth.decorators import login_required
+
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^([0-9]+)/$', ChooseTestView.as_view(), name='choose_test'),
     url(r'^([0-9]+)/([0-9]+)/$', SheetView.as_view(), name='sheet'),
     url(r'^help/$', TemplateView.as_view(template_name='testownik/help.html'), name='help'),
-    url(r'^upload/$', UploadFileView.as_view(), name='upload'),
+    url(r'^upload/$', login_required(UploadFileView.as_view()), name='upload'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'index'}, name='logout'),
     url(r'^create/$', UserCreationView.as_view(), name='create_user'),
     url(r'^pdf/([0-9]+)/([0-9]+)/$', PdfGeneratorView.as_view(), name='pdf')
 ]
-
