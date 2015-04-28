033d1d9503ea8f5a31298b8f4ad44d7d3855c57f
ProjektInzynieriaInternetowa <projekt@ubuntu.(none)>
2015-04-28 01:37:58 -0700

    zmienione timezone w settings

--- TestSystem/settings.py
+++ TestSystem/settings.py
@@ -69,13 +69,13 @@ DATABASES = {
 
 LANGUAGE_CODE = 'pl-eu'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
